6a58e68a3f2de55f68050525b065697f0e3c8330
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-06-06 22:14:50 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,3 +5,21 @@ const bodyEl = document.querySelector('body');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+   bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+})
+
+stopBtn.disabled = true;
