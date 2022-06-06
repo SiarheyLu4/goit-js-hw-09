d250fbe50d2506d7342c81e92767844125873138
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-06-06 20:43:30 +0300

    package.json change

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
